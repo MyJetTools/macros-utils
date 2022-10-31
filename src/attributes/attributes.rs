bdc71533013e5e89cdef70da42b2c43e2f1a54be
amigin <amigin@gmail.com>
2022-10-31 16:12:46 +0200

    Added getting attributes

--- src/attributes/attributes.rs
+++ src/attributes/attributes.rs
@@ -16,4 +16,15 @@ impl Attributes {
     pub fn add(&mut self, key: String, content: Option<Vec<u8>>) {
         self.data.insert(key, AttributeFields::new(content));
     }
+
+    pub fn try_get(&self, name: &str) -> Option<&AttributeFields> {
+        self.data.get(name)
+    }
+
+    pub fn get(&self, name: &str) -> &AttributeFields {
+        match self.try_get(name) {
+            Some(result) => result,
+            None => panic!("Attribute {} is not found", name),
+        }
+    }
 }
