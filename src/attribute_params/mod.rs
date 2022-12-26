850fc0f59f92aadf537c6acb550290fad550a1d0
amigin <amigin@gmail.com>
2022-12-26 17:43:32 +0200

    Debugging

--- src/attribute_params/mod.rs
+++ src/attribute_params/mod.rs
@@ -1,6 +1,6 @@
+mod attr_params;
 mod macro_params_parser;
 mod param_value;
-mod params_parser;
+pub use attr_params::*;
 pub use macro_params_parser::*;
 pub use param_value::*;
-pub use params_parser::*;
